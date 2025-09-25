83fdf7d54ab40a6b3763a0f2efde9efc6399c914
michellebarrigaIT <michelle.barriga@assuresoft.tech>
2025-09-24 21:01:21 -0400

    feat: add props to modal

--- src/Exercise3/pages/LazyModal.tsx
+++ src/Exercise3/pages/LazyModal.tsx
@@ -1,21 +1,54 @@
 import { Suspense, useState, lazy } from "react";
+import { useForm } from "react-hook-form";
 import "./LazyModal.scss";
 
 const Modal = lazy(() => import("../components/Modal"));
 
+type ModalData = {
+    title: string;
+    message: string;
+};
+
 export default function LazyModal() {
     const [open, setOpen] = useState(false);
+
+    const [modalData, setModalData] = useState<ModalData>({
+        title: "",
+        message: "",
+    });
+
+    const { register, handleSubmit, reset } = useForm<ModalData>({
+        defaultValues: modalData,
+    });
+
+    const onSubmit = (data: ModalData) => {
+        setModalData(data);
+        setOpen(true);
+        reset(data);
+    };
+
 
     return (
         <div className="lazy-modal">
             <h1>Lazy Loaded Modal Page</h1>
-            <button onClick={() => setOpen(true)}>Open Modal</button>
+            <form onSubmit={handleSubmit(onSubmit)} className="modal-form">
+                <input
+                    type="text"
+                    placeholder="Modal Title"
+                    {...register("title", { required: true })}
+                />
+                <textarea
+                    placeholder="Modal Message"
+                    {...register("message", { required: true })}
+                    />
+                <button type="submit">Create Modal</button>
+            </form>
 
             {open && (
                 <Suspense fallback={<div>Loading modal...</div>}>
                 <Modal
-                    title="Welcome!"
-                    message="This modal was loaded lazily... zzz"
+                    title={modalData.title}
+                    message={modalData.message}
                     onClose={() => setOpen(false)}
                 />
                 </Suspense>
